ff6c2090ca49221131a0d7a85747edbfa0424524
Moty <36139920+projects22@users.noreply.github.com>
2023-03-26 18:49:11 +0100

    Add files via upload

---
+++ ch341_io/ch341_vb10/Form1.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cb1 = New System.Windows.Forms.CheckBox()
+        Me.cb2 = New System.Windows.Forms.CheckBox()
+        Me.cb3 = New System.Windows.Forms.CheckBox()
+        Me.cb4 = New System.Windows.Forms.CheckBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 21
+        Me.Label3.Text = "driver"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 34)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 20
+        Me.Label2.Text = "open"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(17, 13)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "dll"
+        '
+        'cb1
+        '
+        Me.cb1.AutoSize = True
+        Me.cb1.Checked = True
+        Me.cb1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.cb1.Location = New System.Drawing.Point(215, 12)
+        Me.cb1.Name = "cb1"
+        Me.cb1.Size = New System.Drawing.Size(40, 17)
+        Me.cb1.TabIndex = 36
+        Me.cb1.Text = "D0"
+        Me.cb1.UseVisualStyleBackColor = True
+        '
+        'cb2
+        '
+        Me.cb2.AutoSize = True
+        Me.cb2.Location = New System.Drawing.Point(215, 47)
+        Me.cb2.Name = "cb2"
+        Me.cb2.Size = New System.Drawing.Size(40, 17)
+        Me.cb2.TabIndex = 37
+        Me.cb2.Text = "D1"
+        Me.cb2.UseVisualStyleBackColor = True
+        '
+        'cb3
+        '
+        Me.cb3.AutoSize = True
+        Me.cb3.Location = New System.Drawing.Point(215, 82)
+        Me.cb3.Name = "cb3"
+        Me.cb3.Size = New System.Drawing.Size(40, 17)
+        Me.cb3.TabIndex = 38
+        Me.cb3.Text = "D2"
+        Me.cb3.UseVisualStyleBackColor = True
+        '
+        'cb4
+        '
+        Me.cb4.AutoSize = True
+        Me.cb4.Location = New System.Drawing.Point(215, 117)
+        Me.cb4.Name = "cb4"
+        Me.cb4.Size = New System.Drawing.Size(40, 17)
+        Me.cb4.TabIndex = 39
+        Me.cb4.Text = "D3"
+        Me.cb4.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Red
+        Me.Panel1.Location = New System.Drawing.Point(162, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(14, 14)
+        Me.Panel1.TabIndex = 40
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Red
+        Me.Panel2.Location = New System.Drawing.Point(162, 47)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(14, 14)
+        Me.Panel2.TabIndex = 41
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Red
+        Me.Panel3.Location = New System.Drawing.Point(162, 81)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(14, 14)
+        Me.Panel3.TabIndex = 42
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.Red
+        Me.Panel4.Location = New System.Drawing.Point(162, 117)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(14, 14)
+        Me.Panel4.TabIndex = 43
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 82)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 13)
+        Me.Label4.TabIndex = 44
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(132, 12)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(21, 13)
+        Me.Label5.TabIndex = 45
+        Me.Label5.Text = "D5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(132, 47)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(21, 13)
+        Me.Label6.TabIndex = 46
+        Me.Label6.Text = "D6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(132, 82)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(21, 13)
+        Me.Label7.TabIndex = 47
+        Me.Label7.Text = "D7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(132, 117)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(21, 13)
+        Me.Label8.TabIndex = 48
+        Me.Label8.Text = "D8"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(278, 154)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.cb4)
+        Me.Controls.Add(Me.cb3)
+        Me.Controls.Add(Me.cb2)
+        Me.Controls.Add(Me.cb1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "CH341 Input Output"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cb1 As System.Windows.Forms.CheckBox
+    Friend WithEvents cb2 As System.Windows.Forms.CheckBox
+    Friend WithEvents cb3 As System.Windows.Forms.CheckBox
+    Friend WithEvents cb4 As System.Windows.Forms.CheckBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+
+End Class
---
+++ ch341_io/ch341_vb10/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Threading
+Public Class Form1
+
+    Public Const DLL_PATH As String = "C:\Windows\SysWOW64\CH341DLL.DLL"
+
+
+#Region "Device"
+
+    'ULONG	WINAPI	CH341GetVersion( );  // Get the DLL version number, return the version number
+    <DllImport(DLL_PATH, SetLastError:=True, CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function CH341GetVersion() As Integer
+    End Function
+    'ULONG	WINAPI	CH341GetDrvVersion( );  // Get the driver version number, return the version number, or return 0 if there is an error
+    <DllImport(DLL_PATH, SetLastError:=True, CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function CH341GetDrvVersion() As Integer
+    End Function
+    'HANDLE	WINAPI	CH341OpenDevice(  // Open the CH341 device, return the handle, if an error occurs, it will be invalid
+    'ULONG			iIndex );  // Specify the device serial number of CH341, 0 corresponds to the first device
+    <DllImport(DLL_PATH, SetLastError:=True, CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function CH341OpenDevice(ByVal index As Integer) As IntPtr
+    End Function
+    'VOID	WINAPI	CH341CloseDevice(  // Close the CH341 device
+    'ULONG			iIndex );  // Specify the serial number of the CH341 device
+    <DllImport(DLL_PATH, SetLastError:=True, CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Sub CH341CloseDevice(ByVal index As Integer)
+    End Sub
+    'BOOL	WINAPI	CH341ResetDevice(  // Reset USB device
+    'ULONG			iIndex );  // Specify the serial number of the CH341 device
+    <DllImport(DLL_PATH, SetLastError:=True, CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function CH341ResetDevice(ByVal index As Integer) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+    <DllImport(DLL_PATH, SetLastError:=True, CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function CH341Set_D5_D0(ByVal iIndex As Integer, ByVal iSetDirOut As Integer, ByVal iSetDataOut As Integer) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+    <DllImport(DLL_PATH, SetLastError:=True, CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function CH341SetOutput(ByVal iIndex As Integer, ByVal iEnable As Integer, ByVal iSetDirOut As Integer, ByVal iSetDataOut As Integer) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+    '    BOOL	WINAPI	CH341GetStatus(  // Input data and status directly through CH341
+    '	ULONG			iIndex,  // Specify the serial number of the CH341 device
+    '	PULONG			iStatus );  // Points to a double word unit, used to save status data, refer to the following bit description
+    '// Bit 7-bit 0 correspond to D7-D0 pins of CH341
+    '// Bit 8 corresponds to ERR# pin of CH341, Bit 9 corresponds to PEMP pin of CH341, Bit 10 corresponds to INT# pin of CH341, Bit 11 corresponds to SLCT pin of CH341, Bit 23 corresponds to SDA pin of CH341
+    '// Bit 13 corresponds to BUSY/WAIT# pin of CH341, Bit 14 corresponds to AUTOFD#/DATAS# pin of CH341, Bit 15 corresponds to SLCTIN#/ADDRS# pin of CH341
+    <DllImport(DLL_PATH, SetLastError:=True, CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function CH341GetStatus(ByVal iIndex As Integer, ByRef iStatus As Integer) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+
+    '    BOOL	WINAPI	CH341GetInput(  // Using the CH341 to directly enter data and status is more efficient than using the CH341GetStatus function
+    '	ULONG			iIndex,  // Specify the CH341 device number
+    '	PULONG			iStatus );  // Points to a two-word unit that holds state data, as described in the bit description below
+    '// Bits 7-0 correspond to the D7-D0 pins of CH341
+    '// Bit 8 corresponds to the ERR# pin of CH341, bit 9 to the PEMP pin of CH341, bit 10 to the INT# pin of CH341, bit 11 to the SLCT pin of CH341, and bit 23 to the SDA pin of CH341
+    '// Bit 13 corresponds to the BUSY/WAIT# pin of CH341, bit 14 corresponds to the AUTOFD#/ data # pin of CH341, and bit 15 corresponds to the SLCTIN#/ADDRS# pin of CH341
+    <DllImport(DLL_PATH, SetLastError:=True, CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function CH341GetInput(ByVal iIndex As Integer, ByRef iStatus As Integer) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+
+#End Region
+
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "DLL version: " & CH341GetVersion()
+        If CH341OpenDevice(0) Then
+            Label2.Text = "Device is Open"
+        Else
+            Label2.Text = "Device failed to open"
+        End If
+        'CH341Label2.Text = ResetDevice(0)
+        Label3.Text = "Driver version: " & CH341GetDrvVersion()
+
+        CH341SetOutput(0, 8, 15, 15)    'set D0-D3 as output
+        Timer1.Interval = 100
+        Timer1.Enabled = True
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim out As Byte = 0
+        Dim pins As Integer = 0
+
+        CH341GetInput(0, pins)
+        'Label4.Text = pins And &HF0
+        If pins And &H10 Then Panel1.BackColor = Color.Green Else Panel1.BackColor = Color.Red
+        If pins And &H20 Then Panel2.BackColor = Color.Green Else Panel2.BackColor = Color.Red
+        If pins And &H40 Then Panel3.BackColor = Color.Green Else Panel3.BackColor = Color.Red
+        If pins And &H80 Then Panel4.BackColor = Color.Green Else Panel4.BackColor = Color.Red
+
+        If cb1.Checked = True Then out = 1
+        If cb2.Checked = True Then out = out + 2
+        If cb3.Checked = True Then out = out + 4
+        If cb4.Checked = True Then out = out + 8
+        CH341SetOutput(0, 4, 15, out)   'set outputs
+
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        CH341CloseDevice(0)
+    End Sub
+
+End Class
